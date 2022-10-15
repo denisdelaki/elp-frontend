e5528dc9eb1cc0b65d054bd1f88894421e08cab8
denisdelaki <denisdelaki@gmail.com>
2022-10-16 00:09:37 +0300

    created the AboutUs compopnent

--- src/Components/Aboutus.js
+++ src/Components/Aboutus.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 function Aboutus() {
   return (
-    <div></div>
+    <div>
+          <p className="text-lg relative font-normal -mx-4 p-4 ">Equity Group selects program beneficiaries whom they feel
+             will exploit the opportunity presented for maximum benefit of 
+             not just themselves but the society as well. Each year, the bank </p> <p className="text-lg font-normal">
+             develops a selection-criteria for new scholars based on these factors: </p>
+       <ul className="text-lg font-normal mx-60">
+        <li>General KSCE performance
+       </li>
+       <li>
+           Distribution of Equity Bank Branches
+           </li>
+           <li>
+           Performance of graduating Wings to Fly scholars 
+        </li>
+        <li> 
+        Performance categories i.e. top nationally, top in county and top in sub-county</li>
+        </ul>
+        <div>
+          <div></div> 
+          <div></div> 
+        </div>
+    </div>
   )
 }
 export default Aboutus
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -7,18 +7,45 @@ import Events from '../Components/Events'
 import Hub from '../Components/Hub'
 import Opportunities from '../Components/Opportunities'
 import Aboutus from '../Components/Aboutus'
+import { Routes, Route } from "react-router-dom";
 function Dashboard() {
   return (
     <div> 
         <Sidebar />
         <Header />
-        <div>
-            <Home /> 
-            <Chapters /> 
-            <Events /> 
-            <Opportunities /> 
-            <Hub /> 
-            <Aboutus />
+        <div className="relative left-20 top-2">
+            <Routes>
+        <Route
+          exact
+          path="/"
+          element={<Home />}
+        ></Route>
+        <Route
+          exact
+          path="/chapters"
+          element={<Chapters />}
+        ></Route>
+        <Route
+          exact
+          path="/events"
+          element={<Events />}
+        ></Route>
+        <Route
+          exact
+          path="/opportunities"
+          element={<Opportunities />}
+        ></Route>
+        <Route
+          exact
+          path="/hub"
+          element={<Hub />}
+        ></Route>
+        <Route
+          exact
+          path="/aboutus"
+          element={<Aboutus />}
+        ></Route>
+        </Routes>
          </div>
     </div>
   )
